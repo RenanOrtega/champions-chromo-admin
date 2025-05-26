d66d3c1a53e2863d195a13f18ca44942839ac969
Renan Ortega <renanortega82@gmail.com>
2025-05-26 20:26:00 -0300

    fix: adjust import.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "https://localhost:7133/api",
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 })
